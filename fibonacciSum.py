9544b8d2baf615c24687bf36f825c78b7aecd29d
jaswinder singh <jassidhindsa2019@gmail.com>
2023-08-17 12:42:03 -0400

    fibonacciSum

---
+++ fibonacciSum.py
@@ -0,0 +1,16 @@
+# Return sum of n fibonacci sequence
+
+
+def fibonacciSum(n):    
+    fibonacci_list = [1, 1]
+    result = 0
+
+    for i in range(n):
+        if i <= 1:
+            result += 1
+        else:
+            next_val = fibonacci_list[i-1] +  fibonacci_list[i-2]
+            fibonacci_list.append(next_val)
+            result += next_val
+
+    return result
---
+++ test.py
@@ -0,0 +1,5 @@
+import fibonacciSum
+
+if __name__ == "__main__":
+   val = fibonacciSum.fibonacciSum(6);
+   print(val)
